f1c9c4c99a8d8bb910af8c93b2b4fd4a8f6e002d
241712913 <241712913@students.uajy.ac.id>
2026-06-17 15:34:18 +0700

    uas

--- app/tentang/page.tsx
+++ app/tentang/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import TentangClient from "./tentangClient";
+import TentangClient from "./TentangClient";
 
 export const metadata: Metadata = {
   title: "Tentang SahabatKargo.id",
